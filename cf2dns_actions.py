26566d3efb8c239613b8b8ed808d40e95b7d9d98
StevenNight <stevennight@qq.com>
2021-08-20 21:56:29 +0800

    Update cf2dns_actions.py

--- cf2dns_actions.py
+++ cf2dns_actions.py
@@ -177,5 +177,5 @@ if __name__ == '__main__':
     elif DNS_SERVER == 2:
         cloud = AliApi(SECRETID, SECRETKEY)
         cloud2 = AliApi(SECRETID_ALIYUN, SECRETKEY_ALIYUN)
-    #main(cloud, DOMAINS)
+    main(cloud, DOMAINS)
     main(cloud2, DOMAINS_ALIYUN)
