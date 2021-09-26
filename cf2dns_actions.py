0c3f1116c14d458b03482966321bade683d330f4
stevennight@qq.com <stevennight>
2021-09-26 13:49:19 +0800

    cf2dns_action.py

--- cf2dns_actions.py
+++ cf2dns_actions.py
@@ -64,7 +64,7 @@ def changeDNS(line, s_info, c_info, domain, sub_domain, cloud):
         print("CHANGE DNS ERROR: ----Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----MESSAGE: LINE ERROR")
         return
     try:
-    	create_num = 10
+    	 create_num = 10;
 #        create_num = AFFECT_NUM - len(s_info)
         if create_num == 0:
             for info in s_info:
